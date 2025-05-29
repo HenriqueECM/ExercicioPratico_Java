8f36b76388cd73085b8cb540c200371dcd3c7121
henrique_menel <henrique_menel@SN-400830.edu.fiesc.com.br>
2025-05-29 16:46:50 -0300

    Quase terminando sistema de petShop

---
+++ ProjetoPetshop/src/model/Pessoa.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Pessoa {
+	protected String nome, telefone;
+	
+	public Pessoa(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+}
